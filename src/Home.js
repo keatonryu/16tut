ddca1ed33a424759b68efcc27365ce44525a1973
Keaton Ryu Feliciano <keatonryu@gmail.com>
2025-05-22 00:45:43 -0400

    Chapter 17 Done

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
-const Home = () => {
+import Feed from './Feed';
+
+const Home = ({posts}) => {
   return (
-    <main>
-        <h1>Home</h1>
+    <main className="Home">
+        {posts.length ? (
+            <Feed posts={posts} />
+        ) : (
+            <p style={{marginTop: '2rem'}}>
+              No posts to display.
+            </p>
+        )}
     </main>
   )
 }
 
-export default Home
+export default Home;
