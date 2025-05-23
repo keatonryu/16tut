b5a54143e957695d66bf9d64cc577cf179216349
Keaton Ryu Feliciano <keatonryu@gmail.com>
2025-05-23 00:56:17 -0400

    Chapter 20 Done

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
 import Feed from './Feed';
 
-const Home = ({posts}) => {
+const Home = ({posts, fetchError, isLoading}) => {
   return (
     <main className="Home">
-        {posts.length ? (
+      {isLoading && <p className="statusMsg">Loading posts...</p>}
+      {!isLoading && fetchError && <p className="statusMsg" style={{color: "red"}}>{fetchError}</p>}
+      {!fetchError && !isLoading && (
+        <>
+          {posts.length ? (
             <Feed posts={posts} />
-        ) : (
-            <p style={{marginTop: '2rem'}}>
-              No posts to display.
-            </p>
-        )}
+          ) : (
+            <p className="statusMsg">No posts to display.</p>
+          )}
+        </>
+      )}
     </main>
   )
 }
